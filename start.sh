f54e91eb9fca886a30fdfd524d62e5d6da5a9811
Tuan Le Anh <latuannetnam@gmail.com>
2018-05-21 12:26:21 +0700

    build image before starting

--- start.sh
+++ start.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-docker-compose -f docker-compose.yml up -d
+#echo "Building docker image ..."
+#docker-compose -f docker-compose.yml build
+echo "Starting up ..."
+docker-compose -f docker-compose.yml up -d --build
 # Sleep for 15 seconds waiting for sonarqube to start
 echo "Waiting for sonarqube ...."
 sleep 60
