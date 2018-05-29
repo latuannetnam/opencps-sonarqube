6dfab4dd75929b19da11e13e42e1467ea3603d57
Le Anh Tuan <latuannetnam@gmail.com>
2018-05-29 08:54:37 +0700

    Increase waiting time for sonarqube startup

--- start.sh
+++ start.sh
@@ -5,7 +5,7 @@ echo "Starting up ..."
 docker-compose -f docker-compose.yml up -d --build
 # Sleep for 15 seconds waiting for sonarqube to start
 echo "Waiting for sonarqube ...."
-sleep 60
+sleep 180
 echo "Import pre-defined qualify profile"
 curl -u admin:admin -F 'backup=@assets/SecurityAudit.xml' -v http://localhost:9000/api/qualityprofiles/restore 
 curl -u admin:admin -d 'language=java&qualityProfile=SecurityAudit' -v http://localhost:9000/api/qualityprofiles/set_default 
